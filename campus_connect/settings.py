b5526b60afaaa2ef2985c38ea128bb671e1926cc
Yasir Imran <yasirimran279@gmail.com>
2025-12-30 07:26:08 +0500

    Add Render domain to ALLOWED_HOSTS

--- campus_connect/settings.py
+++ campus_connect/settings.py
@@ -17,8 +17,10 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-temporary-key-change-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,campus-connect-production-8281.up.railway.app').split(',')
-
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS',
+    default='localhost,127.0.0.1,campus-connect-production-8281.up.railway.app,campus-connect-klmu.onrender.com'
+).split(',')
 
 # Application definition
 INSTALLED_APPS = [
